03e34ab32be27ee9fe241b1b88f79ac991a71210
Arve Knudsen <arve.knudsen@gmail.com>
2019-07-04 14:23:57 +0200

    Use own secp256k1 implementation

--- src/keys/index.js
+++ src/keys/index.js
@@ -12,7 +12,7 @@ exports = module.exports
 const supportedKeys = {
   rsa: require('./rsa-class'),
   ed25519: require('./ed25519-class'),
-  secp256k1: require('libp2p-crypto-secp256k1')(keysPBM, require('../random-bytes'))
+  secp256k1: require('@arve.knudsen/libp2p-crypto-secp256k1')(keysPBM, require('../random-bytes'))
 }
 
 exports.supportedKeys = supportedKeys
